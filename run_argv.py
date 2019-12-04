7a3569a6e7fa24e4d2168a6229bf710c79ca6e21
Rashid on Windows <rashidberndt@gmail.com>
2019-12-04 21:09:47 +1300

    example of how to use argv to pass a python script variables by command line

---
+++ run_argv.py
@@ -0,0 +1,18 @@
+#! python3
+#windows
+
+#mac
+#! /usr/bin/env python3
+
+#linux
+#! /usr/bin/python3
+
+#this script expects three arguments separated by a space
+#e.g. python run_argv.py 'one' 'two' '3'
+#note the quotation marks
+import sys
+
+print(sys.argv[0])
+print(sys.argv[1])
+print(sys.argv[2])
+print(sys.argv[3])
